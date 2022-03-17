65521d8fa81ba2b553246d061486b56f97986f74
Sigadong <irwanto.regar30@gmail.com>
2022-03-17 08:26:29 +0700

    exercise temperature conversion app

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,38 +1,9 @@
+import 'dart:io';
+
 void main() {
-  // Exceptions
-
-  // use the on keyword to resolve when an exception occurs.
-/*  try {
-    var a = 7;
-    var b = 0;
-    print(a ~/ b);
-  } on IntegerDivisionByZeroException {
-    print('Can not divide by zero');
-  }*/
-
- // The catch block can be combined with on. Catch will catch if no exception satisfies the attached on block.
-/*  try {
-    var a = 7;
-    var b = 0;
-    print(a ~/ b);
-  } on StringBuffer {
-    print('Can not divide by zero');
-  } catch (e) {
-    print('Exception happened: $e');
-  }*/
-
-  try {
-    var a = 7;
-    var b = 0;
-    print(a ~/ b);
-  } catch (e, s){
-    print('Exception happened: $e');
-    print('Stack trace: $s'); // Kita bisa melihat detail exception dan di baris mana exception tersebut terjadi
-  } finally {
-    // Blok finally akan tetap dijalankan tanpa peduli apa pun hasil yang terjadi pada blok try-catch
-    print('This line still executed');
-  }
-
-
-
+  // Aplikasi Konversi Suhu
+  stdout.write('Masukkan Suhu dalam Fahrenheit: ');
+  var fahrenheit = num.parse(stdin.readLineSync()!);
+  var celsius = (fahrenheit - 32) * 5 / 9;
+  print('$fahrenheit derajat Fahrenheit = $celsius derajat celcius');
 }
