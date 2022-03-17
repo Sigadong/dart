715e703209916cc359ec3afad9838ecadb39c627
Sigadong <irwanto.regar30@gmail.com>
2022-03-17 12:20:12 +0700

    null safety

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,26 +1,24 @@
 void main() {
-  // Optional parameters
+  /*
+   Tanda tanya (?) di atas menunjukkan bahwa variabel favoriteFood boleh memiliki nilai null (nullable).
+   Sementara variabel age harus memiliki nilai angka dan tidak boleh null (non-nullable).
+  */
+  String name = 'John Doe';
+  int age = 23;
+  String? favoriteFood = null;
 
-  // Dengan cara ini Anda bisa memasukkan parameter tanpa mempedulikan urutan parameter dengan menyebutkan nama parameternya.
-  greetNewUser(name: 'Widy', age: 20, isVerified: true);
-  greetNewUser(name: 'Widy', age: 20);
+  buyAMeal(favoriteFood);       // Compile error
 }
 
-// Dart mendukung optional parameter, di mana kita tidak wajib mengisi parameter yang diminta oleh fungsi.
-// Untuk bisa membuat parameter menjadi opsional, kita perlu memasukkannya ke dalam kurung siku.
-// void greetNewUser([String? name, int? age, bool? isVerified]) => print('Hello, My name is $name, my old $age th');
-/*
-  Setiap parameter yang tidak dimasukkan akan memiliki nilai null. Namun sejak versi 2.12, Dart memiliki fitur null safety di mana suatu variabel secara default tidak bisa memiliki nilai null.
-*/
-
-// Solusinya, kita bisa menambahkan tanda tanya (?) setelah tipe data supaya bisa menyimpan nilai null atau memberikan nilai parameter default
-// void greetNewUser([String? name, int? age, bool? isVerified]) => print('Hello, My name is $name, my old $age th');
+void buyAMeal(String? favoriteFood) {
+  if (favoriteFood == null) {
+    print('Bought Nasi Goreng');
+  } else {
+    print('Bought $favoriteFood');
+  }
+}
 
 /*
-Dengan cara ini, urutan parameter masih perlu diperhatikan sehingga jika kita hanya ingin mengisi parameter terakhir,
-kita perlu mengisi parameter sebelumnya dengan null.
-greetNewUser(null, null, true);
+  Cara kedua yang bisa kita lakukan adalah menggunakan bang operator (!).
+  Dengan operator ini kita memberitahu compiler dan memberikan jaminan bahwa variabel tidak akan bernilai null.
 */
-
-// Untuk mengatasi masalah di atas kita bisa memanfaatkan named optional parameters. Pada opsi ini kita menggunakan kurung kurawal pada parameter.
-void greetNewUser({required String name, required int age, bool isVerified = false}) => print('Hello, My name is $name, my old $age th');
