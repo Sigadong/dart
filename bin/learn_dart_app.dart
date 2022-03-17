3bd8852c8a66165f83ff59e2b981db1e9412d0ef
Sigadong <irwanto.regar30@gmail.com>
2022-03-17 07:16:21 +0700

    data types: booleans

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,23 +1,10 @@
 
 void main() {
-  String singleQuote = 'Ini adalah String';
-  String doubleQuote = "Ini juga String";
-  print(singleQuote);
-  print(doubleQuote);
+  bool alwaysTrue = true;
+  var alwaysFalse = false;
 
-  // Strings: use escape string(\)
-  print('"I think it\'s great!" I answered confidently');
-  print("Windows path: C:\\Program Files\\Dart");
-
-  // String Interpolation
-  var name = 'Messi';
-  print('Hello $name, nice to meet you.');
-  print('1 + 1 = ${1 + 1}');
-
-  // using the letter 'r' before the String will tell Dart to treat the String as raw
-  print(r'Dia baru saja membeli komputer seharga $1,000.00');
-  print('Dia baru saja membeli komputer seharga \$1,000.00');
-
-  // In Unicode Darts these are known as runes
-  print('Hi \u2665');
+  // Sign ! above is called the "not" or "bang" operator.
+  // This operator is used to negate boolean values
+  var notTrue = !true;
+  bool notFalse = !false;
 }
