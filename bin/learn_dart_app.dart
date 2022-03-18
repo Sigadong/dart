a12f7a202efb604f75d16091667df6c1a62fd5ce
Sigadong <irwanto.regar30@gmail.com>
2022-03-18 12:11:59 +0700

    infinite loop

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,21 +1,19 @@
 
 /// While and Do-while
 void main() {
-  String username;
-  bool notValid = false;
+  var i = 1;
 
-  do {
-    stdout.write('Masukkan nama Anda (min. 6 karakter): ');
-    username = stdin.readLineSync();
+  while (i < 5) {
+    print(i);
 
-    if (username.length < 6 ) {
-      notValid = true;
-      print('Username Anda tidak valid');
-    }
-  } while (notValid);
+    // i++; //Jika tidak ditulis, menjadi penyebab infinite loops
+  }
 }
 
 /**
-    Kondisi pada while akan dievaluasi sebelum blok kode di dalamnya dijalankan, sedangkan do-while akan mengevaluasi boolean expression setelah blok kodenya dijalankan.
-    Ini artinya kode di dalam do-while akan dijalankan setidaknya satu kali.
+    Ketika menerapkan perulangan pada program kita, ada satu kondisi yang perlu kita hindari yaitu infinite loop.
+    Infinite loop atau endless loop adalah kondisi di mana program kita stucked di dalam perulangan. Ia akan berjalan terus hingga menyebabkan crash pada aplikasi dan komputer kecuali ada intervensi secara eksternal, seperti mematikan aplikasi.
+
+    Dapatkah Anda mencari apa yang salah dari dari kode di atas sehingga terjadi infinite loop??
+    karena variabel i selalu bernilai 1. Alhasil kondisi i < 5 akan selalu bernilai true dan akibatnya aplikasi akan terus mencetak 1 ke konsol sehingga mengalami crash.
 **/
