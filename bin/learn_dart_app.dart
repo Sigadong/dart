17a496d9e9f7636d02f8d671dfa2358321ab1a2f
Sigadong <irwanto.regar30@gmail.com>
2022-03-18 12:03:55 +0700

    while

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,19 +1,15 @@
+
+/// While and Do-while
+void main() {
+  var i = 1;
+
+  while (i <= 100) {
+    print(i);
+    i++;
+  }
+}
+
 /**
- * For Loop
-
-    print(1);
-    print(2);
-    print(3); // sampai ke-10
-    Bagaimana jika kita perlu menampilkan angka 1 sampai 100?
-
-    Sebagai contoh jika kita ingin menampilkan angka 1 sampai 100, kita bisa menuliskan seperti berikut:
-
-    for (int i = 1; i <= 100; i++) {
-      print(i);
-    }
-    Lebih ringkas bukan? Terdapat tiga bagian utama dalam sintaks for di atas:
-
-    Pertama, variabel index yang seringkali diberi nama i yang berarti index. Pada variabel ini kita menginisialisasi nilai awal dari perulangan yang kita lakukan.
-    Kedua, operasi perbandingan. Pada bagian ini komputer akan melakukan pengecekan kondisi apakah perulangan masih perlu dilakukan. Jika bernilai true maka kode di dalam blok for akan dijalankan.
-    Ketiga, increment/decrement. Di sini kita melakukan penambahan atau pengurangan variabel index. Jadi pada contoh di atas variabel indeks akan ditambah dengan 1 di setiap akhir perulangan.
+    Bisa dilihat pada kode di atas bahwa perulangan dengan while tidak memiliki ketergantungan dengan variabel index seperti pada for loop.
+    Karena itu, meskipun while dapat melakukan perulangan yang sama dengan for, WHILE lebih cocok digunakan pada kasus di mana 'kita tidak tahu pasti berapa banyak perulangan yang diperlukan'.
 **/
