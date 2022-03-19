6a4e14c93bf5be17f4c428f8d81f6bc66c57c17c
Sigadong <irwanto.regar30@gmail.com>
2022-03-19 12:28:55 +0700

    collections: Set

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,20 +1,39 @@
 void main() {
-  /* LIST
+  /* SET
    - Spread Operator
-  Dart memiliki fitur menarik untuk menambahkan banyak nilai ke dalam List dengan cara yang singkat, yaitu spread operator.
-  Sesuai namanya “spread”, fitur ini digunakan untuk menyebarkan nilai di dalam collections menjadi beberapa elemen. Spread operator dituliskan dengan tiga titik (...).
+  Set merupakan sebuah collection yang hanya dapat menyimpan nilai yang unik. Ini akan berguna ketika Anda tidak ingin ada data yang sama alias duplikasi dalam sebuah collection.
   */
 
-  var favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
-  var others = ['Cake', 'Pie', 'Donut'];
-  var allFavorites = [...favorites, ...others];
-  print(allFavorites);
+
+  Set<int> anotherSet = new Set.from([1, 4, 6, 4, 1]);
+  print(anotherSet);
+
+  var numberSet = {1, 4, 6};
+
+  // Untuk menambahkan data ke dalam Set kita dapat memanfaatkan fungsi add() atau addAll().
+  numberSet.add(6);
+  numberSet.addAll({2, 2, 3});
   /*
-  Yup, dengan spread operator kita dapat memasukkan masing-masing elemen di dalam List ke dalam List lainnya.
-
-  Untuk mengatasi List yang bisa bernilai null, kita dapat menggunakan null-aware spread operator (...?) seperti berikut:
+  Fungsi add akan menambah satu item ke dalam Set, sementara
+  addAll digunakan untuk menambahkan beberapa item sekaligus.
+  Nilai yang duplikat akan diabaikan.
   */
-  var list; // secara default akan bernilai 'null'
-  var list2 = [0, ...?list];
-  print(list2);
+
+  // Gunakan fungsi remove() untuk menghapus objek di dalam set yang diinginkan. Menghapus nilai 3 di dalam Set, bukan indeks ke-3.
+  numberSet.remove(3);
+
+  // Gunakan fungsi elementAt() untuk menampilkan data pada indeks tertentu.
+  print(numberSet.elementAt(2)); // Output: 6
+
+  print(numberSet);
+
+  /* Selain itu, Dart juga memiliki fungsi union dan intersection untuk mengetahui gabungan dan irisan dari 2 (dua) buah Set.
+    Sebagai contoh:
+  */
+  var setA = {1, 2, 4, 5};
+  var setB = {1, 5, 7};
+  var union = setA.union(setB);
+  var intersection = setA.intersection(setB);
+  print(union); // union: {1, 2, 4, 5, 7}
+  print(intersection); // intersection: {1, 5}
 }
