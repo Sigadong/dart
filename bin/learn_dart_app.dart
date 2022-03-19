cee24d8a50586a6171066cf878dff39fdd0ea01a
Sigadong <irwanto.regar30@gmail.com>
2022-03-19 12:39:22 +0700

    collections: Map

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,37 +1,27 @@
 void main() {
-  /* SET
-  Set merupakan sebuah collection yang hanya dapat menyimpan nilai yang unik. Ini akan berguna ketika Anda tidak ingin ada data yang sama alias duplikasi dalam sebuah collection.
+  /* Map
+  Sebuah collection yang dapat menyimpan data dengan format key-value.
+  String yang berada pada sebelah kiri titik dua(:) adalah sebuah key, sedangkan yang di sebelah kanan merupakan value-nya.
+  Lalu untuk mengakses nilai dari Map tersebut, kita bisa menggunakan key yang sudah dimasukkan.
   */
-  Set<int> anotherSet = new Set.from([1, 4, 6, 4, 1]);
-  print(anotherSet);
+  var capital = {
+    'Jakarta': 'Indonesia',
+    'London': 'England',
+    'Tokyo': 'Japan'
+  };
+  print(capital['Jakarta']); // Output: Indonesia
 
-  var numberSet = {1, 4, 6};
 
-  // Untuk menambahkan data ke dalam Set kita dapat memanfaatkan fungsi add() atau addAll().
-  numberSet.add(6);
-  numberSet.addAll({2, 2, 3});
-  /*
-  Fungsi add akan menambah satu item ke dalam Set, sementara
-  addAll digunakan untuk menambahkan beberapa item sekaligus.
-  Nilai yang duplikat akan diabaikan.
-  */
+  // Kita dapat menampilkan key apa saja yang ada di dalam Map dengan menggunakan property keys.
+  var mapKeys = capital.keys; // mapKeys = (Jakarta, London, Tokyo)
+  print(mapKeys);
 
-  // Gunakan fungsi remove() untuk menghapus objek di dalam set yang diinginkan. Menghapus nilai 3 di dalam Set, bukan indeks ke-3.
-  numberSet.remove(3);
+  // Sedangkan untuk mengetahui nilai apa saja yang ada di dalam Map kita bisa menggunakan property values.
+  var mapValues = capital.values; // mapValues = (Indonesia, England, Japan)
+  print(mapValues);
 
-  // Gunakan fungsi elementAt() untuk menampilkan data pada indeks tertentu.
-  print(numberSet.elementAt(2)); // Output: 6
-
-  print(numberSet);
-
-  /* Selain itu, Dart juga memiliki fungsi union dan intersection untuk mengetahui gabungan dan irisan dari 2 (dua) buah Set.
-    Sebagai contoh:
-  */
-  var setA = {1, 2, 4, 5};
-  var setB = {1, 5, 7};
-  var union = setA.union(setB);
-  var intersection = setA.intersection(setB);
-  print(union); // union: {1, 2, 4, 5, 7}
-  print(intersection); // intersection: {1, 5}
+  // Untuk menambahkan key-value baru ke dalam Map, kita bisa melakukannya dengan cara berikut:
+  capital['New Delhi'] = 'India';
+  print(capital); // Output: {Jakarta: Indonesia, London: England, Tokyo: Japan, New Delhi: India}
 
 }
