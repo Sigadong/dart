42373d7d2d72e432148e06ffc1773a8451bb7ad9
Sigadong <irwanto.regar30@gmail.com>
2022-03-19 12:58:21 +0700

    paradigma OOP

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,33 +1,8 @@
 void main() {
-  /* Map
-  Sebuah collection yang dapat menyimpan data dengan format key-value.
-  String yang berada pada sebelah kiri titik dua(:) adalah sebuah key, sedangkan yang di sebelah kanan merupakan value-nya.
-  Lalu untuk mengakses nilai dari Map tersebut, kita bisa menggunakan key yang sudah dimasukkan.
+  /*Paradigma Object Oriented Programming
+    - Encapsulation
+    - Abstraction
+    - Inheritance
+    - Polymorphism
   */
-  var capital = {
-    'Jakarta': 'Indonesia',
-    'London': 'England',
-    'Tokyo': 'Japan'
-  };
-  print(capital['Jakarta']); // Output: Indonesia
-
-
-  // Kita dapat menampilkan key apa saja yang ada di dalam Map dengan menggunakan property keys.
-  var mapKeys = capital.keys; // mapKeys = (Jakarta, London, Tokyo)
-  print(mapKeys);
-
-  // Sedangkan untuk mengetahui nilai apa saja yang ada di dalam Map kita bisa menggunakan property values.
-  var mapValues = capital.values; // mapValues = (Indonesia, England, Japan)
-  print(mapValues);
-
-  // Untuk menambahkan key-value baru ke dalam Map, kita bisa melakukannya dengan cara berikut:
-  capital['New Delhi'] = 'India';
-  print(capital); // Output: {Jakarta: Indonesia, London: England, Tokyo: Japan, New Delhi: India}
-
-  // Untuk menghapus seluruh key-value yang ada di dalam Map bisa menggunakan fungsi clear().
-  var mapClear = capital.clear();
-
-  // Saat mengakses key yang tidak ada pada map, nilai variabel akan bernilai null.
-  print(capital['New Delhi']);
-
 }
