e65864d8180cec44cb42590f4dd7e4ae08cd11e1
Sigadong <irwanto.regar30@gmail.com>
2022-03-20 10:49:21 +0700

    cascade notation

--- bin/Animal.dart
+++ bin/Animal.dart
@@ -1,44 +1,22 @@
-/* Named Constructor
-  Pada beberapa kasus kita mungkin akan membutuhkan beberapa constructor untuk skenario yang berbeda-beda.
-  Pada situasi ini kita bisa memanfaatkan named constructor.
-
-  Dengan menggunakan named constructor, kita dapat membuat beberapa constructor pada kelas yang sama.
-  Setiap constructor akan memiliki nama yang unik.
-*/
-
 class Animal {
-  String _name = '';
-  int _age = 0;
-  double _weight = 0;
+  String name = '';
+  int age = 0;
+  double weight = 0;
 
-  /* Named Constructor
-    class_name.constructor_name (arguments){
-      // Statements
-    }
-  */
-  Animal.Name(this._name);
-  Animal.Age(this._age);
-  Animal.Weight(this._weight);
+  Animal(this.name, this.age, this.weight);
 
-  // Setter
-  set name(String value) {
-    _name = value;
-  }
-
-// Getter
-  double get weight => _weight;
 
   void eat() {
-    print('$_name is eating.');
-    _weight = _weight + 0.2;
+    print('$name is eating.');
+    weight = weight + 0.2;
   }
 
   void sleep() {
-    print('$_name is sleeping.');
+    print('$name is sleeping.');
   }
 
   void poop() {
-    print('$_name is pooping.');
-    _weight = _weight - 0.1;
+    print('$name is pooping.');
+    weight = weight - 0.1;
   }
 }
--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,9 +1,24 @@
 import 'Animal.dart';
 
-void main() {
-  var dicodingCat = Animal.Name('irwan');
+/* Cascade Notation
+  Dart juga dilengkapi dengan cascade notation atau cascade operator.
+  Operator ini memungkinkan kita untuk melakukan beberapa urutan operasi pada objek yang sama.
+  Kita bisa mengakses property dari object dan menjalankan method di dalamnya bersamaan ketika kita menginstansiasi object.
+  Cascade operator dituliskan dengan dua tanda titik (.. atau ?..).
+*/
 
-  dicodingCat.eat();
-  dicodingCat.poop();
-  print(dicodingCat.weight);
+void main() {
+  var dicodingCat = Animal('', 2, 4.2)
+    ..name = 'Gray'
+    ..eat();
 }
+
+/*
+  Apakah Anda mengertide N maksud kode di atas? Kita menginstansiasi object Animal dengan constructor seperti biasa.
+  Kemudian cascade operator yang mengikutinya akan melakukan operasi berdasarkan object yang dikembalikan oleh constructor.
+  Contoh kode tersebut melakukan hal yang sama apabila kita menuliskan kode seperti ini:
+
+    var dicodingCat = Animal('', 2, 4.2);
+    dicodingCat.name = 'Gray';
+    dicodingCat.eat();
+*/
