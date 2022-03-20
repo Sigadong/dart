f87bd501e7fbf8fad6817f7b2bab7c7879339970
Sigadong <irwanto.regar30@gmail.com>
2022-03-20 13:45:45 +0700

    implicit interface

--- bin/Bird.dart
+++ bin/Bird.dart
@@ -10,4 +10,14 @@ class Bird extends Animal implements Flyable {
   String featherColor;
 
   Bird(String name, int age, double weight, this.featherColor) : super(name, age, weight);
+
+  @override
+  void fly() {
+    print('$name is flying');
+  }
 }
+
+/*
+  Keyword atau anotasi @override menunjukkan fungsi tersebut mengesampingkan fungsi yang ada di interface atau kelas induknya,
+  lalu menggunakan fungsi yang ada dalam kelas itu sendiri sebagai gantinya.
+*/
