c880d3993d905589f55a2355a165c98ce7a168b1
Sigadong <irwanto.regar30@gmail.com>
2022-03-21 19:16:47 +0700

    pure functions

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -5,8 +5,20 @@ penulisan syntax Dart juga menggunakan gaya functional programming.
 
 Functional programming adalah paradigma pemrograman di mana proses komputasi didasarkan pada fungsi matematika murni.
 Functional programming (FP) ditulis dengan gaya deklaratif yang berfokus pada “what to solve” dibandingkan “how to solve” pada gaya imperatif.
+
+Berikut ini beberapa konsep dan karakteristik functional programming:
+
+Pure functions
+Pure functions berarti sebuah fungsi bergantung pada argumen atau parameter yang dimasukkan ke dalamnya. Sehingga pemanggilan fungsi dengan
+nilai argumen yang sama akan selalu memberikan hasil yang sama pula.
+Contohnya pada fungsi sum() berikut nilai yang dikembalikan akan bergantung pada argumen yang diberikan.
 */
-
 void main() {
+  /**/
 
+  int sum(int num1, int num2) {
+    return num1 + num2;
+  }
+
+  /**/
 }
