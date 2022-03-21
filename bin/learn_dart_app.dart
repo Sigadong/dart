71a17d08f17043e8745c3c342e4cd087c6c0ad6e
Sigadong <irwanto.regar30@gmail.com>
2022-03-21 19:20:42 +0700

    recurtion

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -8,17 +8,23 @@ Functional programming (FP) ditulis dengan gaya deklaratif yang berfokus pada â€
 
 Berikut ini beberapa konsep dan karakteristik functional programming:
 
-Pure functions
-Pure functions berarti sebuah fungsi bergantung pada argumen atau parameter yang dimasukkan ke dalamnya. Sehingga pemanggilan fungsi dengan
-nilai argumen yang sama akan selalu memberikan hasil yang sama pula.
-Contohnya pada fungsi sum() berikut nilai yang dikembalikan akan bergantung pada argumen yang diberikan.
+Recursion
+Pada functional programming tidak ada konsep perulangan seperti for atau while.
+Iterasi pada functional programming dilakukan melalui rekursi atau pemanggilan fungsi dari fungsi itu sendiri,
+hingga mencapai kasus dasar.
 */
-void main() {
-  /**/
 
-  int sum(int num1, int num2) {
-    return num1 + num2;
+int fibonacci(n) {
+  if (n <= 0) {
+    return 0;
+  } else if(n == 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
   }
+}
 
-  /**/
+
+void main() {
+  fibonacci(7);
 }
