3323f758dce91ebee003c82fc34237b2fc8ffe58
Sigadong <irwanto.regar30@gmail.com>
2022-03-21 19:46:21 +0700

    higher-order functions.

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,24 +1,34 @@
 /* Higher-Order Functions
-Apa yang bisa dilakukan dengan lambda atau anonymous function??
+Jika disimulasikan fungsi myHigherOrderFunction akan memanggil fungsi sum yang dijadikan parameter.
 
-Kita bisa memanfaatkan lambda untuk membuat higher-order function.
-Higher order function adalah fungsi yang menggunakan fungsi lainnya sebagai parameter, menjadi tipe kembalian, atau keduanya.
-
-Seperti fungsi berikut:
   void myHigherOrderFunction(String message, Function myFunction) {
     print(message);
-    print(myFunction(3, 4));
+    print(myFunction(3, 4));  // sum(3, 4)    // return 3 + 4
   }
-Fungsi di atas merupakan higher order function karena menerima parameter berupa fungsi lain. Untuk memanggil fungsi di atas,
-kita bisa langsung memasukkan lambda sebagai parameter maupun variabel yang berisi nilai berupa fungsi.
+
+Namun deklarasi higher order function ini bisa menjadi sedikit tricky. Misalnya kode di bawah ini tidak akan
+terdeteksi eror namun ketika dijalankan, aplikasi Anda akan mengalami crash. Tahukah kenapa?
+
+  void myHigherOrderFunction(String message, Function myFunction) {
+    print(message);
+    print(myFunction(4));
+  }
+
+Karena kita tidak menentukan spesifikasi dari fungsi seperti jumlah parameter atau nilai kembaliannya, maka semua
+jenis fungsi akan bisa dijalankan termasuk pemanggilan myFunction seperti di atas. Untuk mengatasinya kita bisa lebih
+spesifik menentukan seperti apa fungsi yang valid untuk menjadi parameter.
+
+  void myHigherOrderFunction(String message, int Function(int num1, int num2) myFunction) { }
+
+Pada fungsi di atas kita perlu memasukkan fungsi dengan dua parameter dan nilai kembali berupa int sebagai parameter.
 */
 
 void main() {
-  // Opsi 1
-  Function sum = (int num1, int num2) => num1 + num2;
-  myHigherOrderFunction('Hello', sum);
+  // Satu fungsi yang merupakan higher order function yaitu fungsi forEach().
+  var fibonacci = [0, 1, 1, 2, 3, 5, 8, 13];
 
+  fibonacci.forEach((item) {
+    print(item);
+  });
 
-  // Opsi 2
-  myHigherOrderFunction('Hello', (num1, num2) => num1 + num2);
 }
