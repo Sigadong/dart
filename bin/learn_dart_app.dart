17879af7956c10412d99f1bd59a851e6d2649e6c
Sigadong <irwanto.regar30@gmail.com>
2022-03-21 15:13:01 +0700

    Mixins

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,49 +1,13 @@
-/*
-  Enumerated Types
-  Bagaimana kita bisa menyimpan banyak nilai konstan di satu tempat dan menanganinya secara bersamaan??
-  Solusinya, Dart menyediakan Enums. Enum mewakili kumpulan konstan yang membuat kode kita lebih jelas dan mudah dibaca.
+/*Mixins
+  Mixin adalah cara menggunakan kembali kode kelas dalam banyak hirarki kelas.
 
-  Enum pada Dart memiliki beberapa property bawaan yang dapat kita gunakan untuk menampilkan seluruh nilai dalam bentuk list
-  serta menampilkan item dan indeks dari item tersebut.
+  Kita memiliki superclass Animal dengan tiga subclass. Di bawahnya ada beberapa kelas
+  turunan yang memiliki perilaku berbeda-beda. Beberapa hewan memiliki perilaku yang sama,
+  seperti Cat dan Duck sama-sama bisa berjalan. Kita bisa saja membuat kelas seperti Walkable, Swimmable, dan Flyable.
+  Sayangnya, Dart tidak mendukung multiple inheritance, sehingga sebuah kelas hanya bisa mewarisi (inherit) satu kelas induk. Kita bisa saja membuat interface lalu mengimplementasikannya ke ke kelas Cat atau Duck.
+  Namun, implementasi interface mengharuskan kita untuk meng-override method dan membuat implementasi fungsi di masing-masing kelas.
 */
 
 void main(){
-  enum Rainbow {
-    red, orange, yellow, green, blue, indigo, violet
-  }
 
-  enum Status {
-    Todo, In_Progress, In_Review, Done
-  }
-
-  print(Rainbow.values);
-  print(Rainbow.blue);
-  print(Rainbow.orange.index);
-
-  /*
-  Ketika kode di atas dijalankan, maka konsol akan tampil seperti berikut:
-  Output:
-    [Rainbow.red, Rainbow.orange, Rainbow.yellow, Rainbow.green, Rainbow.blue, Rainbow.indigo, Rainbow.violet]
-    Rainbow.blue
-    1
-
-  Kita juga bisa menggunakan enum ke dalam switch statements.
-  Namun kita perlu menangani semua kemungkinan nilai enum yang Ada.
-  */
-  var taskStatus = Status.In_Progress;
-
-  switch(taskStatus) {
-    case Status.Todo:
-    print('Task is still in To do');
-    break;
-    case Status.In_Progress:
-    print('Task is in progress');
-    break;
-    case Status.In_Review:
-    print('Task is currently under review');
-    break;
-    case Status.Done:
-    print('Task is done');
-    break;
-  }
 }
