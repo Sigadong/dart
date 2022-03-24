3c1ee7a9a69a91784a6581042ad3e4895e20a58d
Sigadong <irwanto.regar30@gmail.com>
2022-03-24 11:46:03 +0700

    pengenalan dart type system

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,27 +1,22 @@
-/* Closures
-Suatu fungsi dapat dibuat dalam lingkup global atau di dalam fungsi lain.
-Suatu fungsi yang dapat mengakses variabel di dalam lexical scope-nya disebut dengan closure.
-Lexical scope berarti bahwa pada sebuah fungsi bersarang (nested functions), fungsi yang berada di dalam memiliki akses ke variabel di lingkup induknya.
+/* Pengenalan Dart Type System
+Dalam bahasa pemrograman, type system adalah sistem logis yang terdiri dari seperangkat aturan yang menetapkan properti atau tipe ke berbagai
+konstruksi program komputer, seperti variabel, expression, fungsi, atau modul.
+Type system ini memformalkan atau memberikan standar kategori tersirat yang digunakan programmer untuk tipe data, struktur data, atau komponen lainnya.
 
-Berikut ini adalah contoh kode implementasi closure:
-*/
+Dart menyebut type system-nya sebagai sound type system. Soundness ini berarti program Anda tidak akan pernah bisa memasuki keadaan di mana sebuah
+ekspresi mengevaluasi nilai yang tidak cocok dengan jenis tipenya.
 
-void main() {
-  var closureExample = calculate(2);
-  closureExample();
-  closureExample();
-}
+Sound type system pada Dart ini sama dengan type system pada Java atau C#. Di mana kondisi soundness ini dicapai dengan menggunakan kombinasi
+pemeriksaan statis (compile-time error) dan pemeriksaan saat runtime. Sebagai contoh, menetapkan String ke variabel int adalah kesalahan compile-time.
+Casting Object ke String dengan as String akan gagal ketika runtime jika objek tersebut bukan String.
 
-Function calculate(base) {
-  var count = 1;
-
-  return () => print("Value is ${base + count++}");
-}
-
-/*
-  Di dalam fungsi calculate() terdapat variabel count dan mengembalikan nilai berupa fungsi.
-  Fungsi lambda di dalamnya memiliki akses ke variabel count karena berada pada lingkup yang sama.
-  Karena variabel count berada pada scope calculate, maka umumnya variabel tersebut akan hilang atau dihapus ketika fungsinya selesai dijalankan.
-  Namun pada kasus di atas fungsi lambda atau closureExample masih memiliki referensi atau akses ke variabel count sehingga bisa diubah.
-  Variabel pada mekanisme di atas telah tertutup (close covered), yang berarti variabel tersebut berada di dalam closure.
+Manfaat dari sound type system ini, antara lain:
+- Mengungkap bug terkait tipe pada saat compile time.
+  Sound type system memaksa kode untuk tidak ambigu tentang tipenya, sehingga bug terkait tipe yang mungkin sulit ditemukan saat runtime, bisa ditemukan pada waktu kompilasi.
+- Kode lebih mudah dibaca.
+  Kode menjadi lebih mudah dibaca karena Anda dapat mengandalkan nilai yang benar-benar memiliki tipe yang ditentukan. Tipe pada Dart tidak bisa berbohong.
+- Kode lebih mudah dikelola.
+  Ketika Anda mengubah satu bagian kode, type system dapat memperingatkan Anda tentang bagian kode mana yang baru saja rusak.
+- Kompilasi ahead of time (AOT) yang lebih baik.
+  Kode yang dihasilkan saat kompilasi AOT menjadi jauh lebih efisien.
 */
