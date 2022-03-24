4140ee631ea3aa51ff6761037fdab79625e225ba
Sigadong <irwanto.regar30@gmail.com>
2022-03-24 13:37:51 +0700

    uncompleted

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,23 +1,13 @@
-/* Apa itu Future
-Untuk melakukan pemrograman secara asynchronous dengan Dart, kita menggunakan Future.
-Future adalah sebuah objek yang mewakili nilai potensial atau kesalahan yang akan tersedia pada suatu waktu di masa
-mendatang. Anda dapat membayangkan future sebagai sebuah kotak paket.
-Saat Anda menerima paket tersebut, akan ada tiga kondisi yang mungkin terjadi, antara lain paket masih tertutup (uncompleted),
-paket dibuka lalu berisi barang sesuai pesanan (completed with data), dan paket dibuka namun terjadi kesalahan atau tidak
-sesuai (completed with error). Penerima dari future dapat menentukan callbacks yang akan menangani nilai atau kesalahan tersebut.
-*/
-
-void main() {
-  final myFuture = Future(() {
-    print('Creating the future');
-    return 12;
+// Sebuah fungsi yang mengembalikan nilai Future.
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    return 'Coffee Boba';
   });
-  print('main() done');
 }
 
 /*
-Jika kode di atas dijalankan, seluruh fungsi main akan dieksekusi sebelum fungsi yang ada di dalam Future(). Ini disebabkan karena future masih berstatus uncompleted. Sehingga ketika program dijalankan, konsol akan tampil seperti berikut:
-  main() done
-  Creating the future
-Lantas bagaimana caranya kita membuat dan memanfaatkan future ini?
+Pada Future kita bisa menambahkan method delayed untuk menunda eksekusi kode di dalam Future.
+Di mana parameter pertama berisi durasi penundaan dan parameter kedua adalah blok kode atau fungsi yang akan dijalankan.
+Pada kasus ini kita menggunakan delayed untuk menunda waktu eksekusi seolah kita sedang mengambil data dari internet.
+Karena nilai yang dikembalikan pada Future berupa String, kita bisa secara eksplisit menentukan tipe parameter generic Future<String>.
 */
