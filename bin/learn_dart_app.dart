d57cdc81020d49c7cb58810c305eb99a34c74fff
Sigadong <irwanto.regar30@gmail.com>
2022-03-24 13:35:07 +0700

    apa itu future

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,16 +1,23 @@
-/* Pengenalan Dart FuturesSynchronous vs Asynchronous
+/* Apa itu Future
+Untuk melakukan pemrograman secara asynchronous dengan Dart, kita menggunakan Future.
+Future adalah sebuah objek yang mewakili nilai potensial atau kesalahan yang akan tersedia pada suatu waktu di masa
+mendatang. Anda dapat membayangkan future sebagai sebuah kotak paket.
+Saat Anda menerima paket tersebut, akan ada tiga kondisi yang mungkin terjadi, antara lain paket masih tertutup (uncompleted),
+paket dibuka lalu berisi barang sesuai pesanan (completed with data), dan paket dibuka namun terjadi kesalahan atau tidak
+sesuai (completed with error). Penerima dari future dapat menentukan callbacks yang akan menangani nilai atau kesalahan tersebut.
+*/
 
-Dalam synchronous program, kode program dijalankan secara berurutan dari atas ke bawah.
-Artinya jika kita menuliskan dua baris kode maka baris kode kedua tidak bisa dieksekusi sebelum kode baris pertama selesai dieksekusi.
-Kita bisa bayangkan ketika sedang berada dalam antrian kasir. Kita tidak akan dilayani sebelum semua antrian di depan kita selesai dilayani,
-begitu pula orang di belakang kita pun harus menunggu gilirannya.
+void main() {
+  final myFuture = Future(() {
+    print('Creating the future');
+    return 12;
+  });
+  print('main() done');
+}
 
-Dalam asynchronous program, jika kita menuliskan dua baris kode, kita dapat membuat baris kode kedua dieksekusi tanpa harus menunggu kode pada
-baris pertama selesai dieksekusi.
-Dalam dunia nyata bisa kita bayangkan dengan memesan kopi melalui pelayan, di mana sembari menunggu pesanan kita datang,
-kita dapat melakukan aktivitas lain seperti membuka laptop atau menulis.
-
-Program asynchronous memungkinkan suatu operasi bisa berjalan sembari menunggu operasi lainnya selesai.
-Umumnya kita memanfaatkan asynchronous pada operasi yang besar dan membutuhkan waktu lama, seperti mengambil data dari internet
-atau API, menyimpan data ke database, dan membaca data dari sebuah berkas.
+/*
+Jika kode di atas dijalankan, seluruh fungsi main akan dieksekusi sebelum fungsi yang ada di dalam Future(). Ini disebabkan karena future masih berstatus uncompleted. Sehingga ketika program dijalankan, konsol akan tampil seperti berikut:
+  main() done
+  Creating the future
+Lantas bagaimana caranya kita membuat dan memanfaatkan future ini?
 */
