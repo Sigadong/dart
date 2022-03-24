3759a43796bacb855784cb656c82b38348fbf86b
Sigadong <irwanto.regar30@gmail.com>
2022-03-24 13:28:50 +0700

    pengenalan dart-futures

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,30 +1,16 @@
-/* Type Inference
-Dart mendukung type inference. Dart memiliki analyzer yang dapat menentukan menyimpulkan tipe untuk field, method,
-variabel lokal, dan beberapa tipe argumen generic. Ketika analyzer tidak memiliki informasi yang cukup untuk menyimpulkan
-tipe tertentu, maka tipe dynamic akan digunakan.
+/* Pengenalan Dart FuturesSynchronous vs Asynchronous
 
-Misalnya berikut ini adalah contoh penulisan variabel map dengan tipe yang eksplisit:
-  Map<String, dynamic> company = {'name': 'Dicoding', 'yearsFounded': 2015};
+Dalam synchronous program, kode program dijalankan secara berurutan dari atas ke bawah.
+Artinya jika kita menuliskan dua baris kode maka baris kode kedua tidak bisa dieksekusi sebelum kode baris pertama selesai dieksekusi.
+Kita bisa bayangkan ketika sedang berada dalam antrian kasir. Kita tidak akan dilayani sebelum semua antrian di depan kita selesai dilayani,
+begitu pula orang di belakang kita pun harus menunggu gilirannya.
 
-Atau, Anda dapat menggunakan var dan Dart akan menentukan tipenya.
-  var company = {'name': 'Dicoding', 'yearsFounded': 2015}; // Map<String, Object>
+Dalam asynchronous program, jika kita menuliskan dua baris kode, kita dapat membuat baris kode kedua dieksekusi tanpa harus menunggu kode pada
+baris pertama selesai dieksekusi.
+Dalam dunia nyata bisa kita bayangkan dengan memesan kopi melalui pelayan, di mana sembari menunggu pesanan kita datang,
+kita dapat melakukan aktivitas lain seperti membuka laptop atau menulis.
 
-Type inference menentukan tipe dari entri kemudian menentukan tipe dari variabelnya.
-Pada contoh di atas, kedua key dari map adalah String, namun nilainya memiliki tipe yang berbeda, yaitu String
-dan int, di mana keduanya merupakan turunan dari Object. Sehingga variabel company akan memiliki tipe Map<String, Object>.
-
-Saat menetapkan nilai objek ke dalam objek lain, kita bisa mengganti tipenya dengan tipe yang berbeda tergantung
-pada apakah objek tersebut adalah consumer atau producer. Perhatikan assignment berikut:
-  Fish fish = Fish();
-
-Fish fish adalah consumer dan Fish() adalah producer. Pada posisi consumer, aman untuk mengganti consumer bertipe
-yang spesifik dengan tipe yang lebih umum. Jadi, aman untuk mengganti Fish fish dengan Animal fish karena Animal adalah supertype dari Fish.
-  Animal fish = Fish();
-
-Namun mengganti Fish fish dengan Shark fish melanggar type safety karena bisa saja Fish memiliki subtype lain dengan
-perilaku berbeda, misalnya FlyingFish.
-  Shark fish = Fish();  // Error
-
-Pada posisi producer, aman untuk mengganti tipe yang umum (supertype) dengan tipe yang lebih spesifik (subtype).
-  Fish fish = Shark();
+Program asynchronous memungkinkan suatu operasi bisa berjalan sembari menunggu operasi lainnya selesai.
+Umumnya kita memanfaatkan asynchronous pada operasi yang besar dan membutuhkan waktu lama, seperti mengambil data dari internet
+atau API, menyimpan data ke database, dan membaca data dari sebuah berkas.
 */
