5d09b7662cc2a149f5fcabd7d1d2ce59aa50ffdf
Sigadong <irwanto.regar30@gmail.com>
2022-03-25 09:23:28 +0700

    completed with data

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,13 +1,16 @@
-// Sebuah fungsi yang mengembalikan nilai Future.
-Future<String> getOrder() {
-  return Future.delayed(Duration(seconds: 3), () {
-    return 'Coffee Boba';
+// Kemudian setelah Future dijalankan, kita memerlukan handler untuk menangani status completed with data.
+// Caranya gunakan method .then() dari objek Future.
+
+void main() {
+  getOrder().then((value) {
+    print('You order: $value');
   });
+  print('Getting your order...');
 }
 
 /*
-Pada Future kita bisa menambahkan method delayed untuk menunda eksekusi kode di dalam Future.
-Di mana parameter pertama berisi durasi penundaan dan parameter kedua adalah blok kode atau fungsi yang akan dijalankan.
-Pada kasus ini kita menggunakan delayed untuk menunda waktu eksekusi seolah kita sedang mengambil data dari internet.
-Karena nilai yang dikembalikan pada Future berupa String, kita bisa secara eksplisit menentukan tipe parameter generic Future<String>.
+Fungsi getOrder() akan dijalankan secara asynchronous hingga setelah 3 detik kode Future akan dijalankan dan mengembalikan nilai.
+Output:
+  Getting your order...
+  You order: Coffee Boba  // Muncul setelah 3 detik
 */
