cd10cef4398806bef797eea8206ade222548a3cd
Sigadong <irwanto.regar30@gmail.com>
2022-03-25 09:29:00 +0700

    completed with error

--- bin/learn_dart_app.dart
+++ bin/learn_dart_app.dart
@@ -1,16 +1,31 @@
-// Kemudian setelah Future dijalankan, kita memerlukan handler untuk menangani status completed with data.
-// Caranya gunakan method .then() dari objek Future.
+/* Completed with error
+  Kita dapat menambahkan method .catchError() setelah then. Sehingga ketika
+  terjadi eror atau exception di dalam Future, blok kode ini akan dijalankan.
+*/
 
 void main() {
   getOrder().then((value) {
     print('You order: $value');
+  })
+      .catchError((error) {
+    print('Sorry. $error');
   });
   print('Getting your order...');
 }
 
+Future<String> getOrder() {
+  return Future.delayed(Duration(seconds: 3), () {
+    var isStockAvailable = false;
+    if (isStockAvailable) {
+      return 'Coffee Boba';
+    } else {
+      throw 'Our stock is not enough.';
+    }
+  });
+}
 /*
-Fungsi getOrder() akan dijalankan secara asynchronous hingga setelah 3 detik kode Future akan dijalankan dan mengembalikan nilai.
-Output:
-  Getting your order...
-  You order: Coffee Boba  // Muncul setelah 3 detik
+Seperti pada fungsi main() ada tiga blok kode yang mewakili state Future:
+  1.Fungsi getOrder() yang berisi Future yang masih uncompleted.
+  2.Method then() yang menangani kondisi completed with data.
+  3.Method catchError() yang menangani kondisi completed with error.
 */
